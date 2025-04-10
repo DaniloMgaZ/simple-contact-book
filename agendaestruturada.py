79cb383b09a4088a58ec4ba333bba53ad3e47168
Danilo Fernandes <ceusouop@gmail.com>
2025-04-10 16:35:32 -0300

    Update agendaestruturada.py

--- agendaestruturada.py
+++ agendaestruturada.py
@@ -46,45 +46,56 @@ def printContacts():
         i+=1            
 
 def removeContacts():
+    stop = 0
     print(25*"-","Remover Contato",25*"-")
     printContacts()
-    remove = int(input("Digite a posição que você deseja remover: "))
-    remove = remove - 1
-    temporary = lista[remove]
-    name = temporary.get("name")
-    number = temporary.get("number")
-    del lista[remove]
-    print(25*"-",f"\n {remove+1}°\n\n Contato: {name}\n Número: {number}\n foi Removido PERMANENTEMENTE!.\n",25*"-")
-    loop()
+    while stop != '':
+        remove = int(input("\n(0) Voltar\nDigite a posição que você deseja remover: "))
+        if remove == 0:
+            loop()
+            break
+        elif remove != len(lista):
+            print("Não existe essa posição")
+        else:    
+            remove = remove - 1
+            temporary = lista[remove]
+            name = temporary.get("name")
+            number = temporary.get("number")
+            del lista[remove]
+            print(25*"-",f"\n {remove+1}°\n\n Contato: {name}\n Número: {number}\n foi Removido PERMANENTEMENTE!.\n",25*"-")
+            loop()
+            break
 
 def editContacts():
     stop = 0
     print(25*"-","Editar Contato",25*"-")
     printContacts()
     while stop != '':
-        edit = int(input("Qual posição você deseja remover: "))
-        if edit != len(lista):
-            print("Essa posição não existe!.")
+        edit = int(input("\n(0) Voltar\nQual posição você deseja editar: "))
+        if edit == 0:
+            loop()
+            break
+        elif edit != len(lista):
+            print("Essa posição não existe")
         else:
             edit = edit - 1
             editing = lista[edit]
-            break
-    while stop != '':
-        what = int(input("O que você deseja editar neste contato: \n\n (1) - Nome \n (2) - Número:  \n (3) - Voltar \n:"))
-        match what:
-            case 1:
-                newName = input("Digite o novo nome: ")
-                editing["name"] = newName
-            case 2:
-                newNumber = input("Digite o novo número: ")
-                editing["number"] = newNumber
-            case 3:
-                print("Voltando")
-                break
-    loop()
+            while stop != '':
+                what = int(input("O que você deseja editar neste contato: \n\n (1) - Nome \n (2) - Número:  \n (3) - Voltar \n:"))
+                match what:
+                    case 1:
+                        newName = input("Digite o novo nome: ")
+                        editing["name"] = newName
+                    case 2:
+                        newNumber = input("Digite o novo número: ")
+                        editing["number"] = newNumber
+                    case 3:
+                        print("Voltando")
+                        break
 
 def loop():
     stop = 0
+    i = 0
     while stop != '':
         print(25*"-","AGENDA",25*"-","\n\n (1) - Criar Contato \n (2) - Listar Contato \n (3) - Remover Contato \n (4) - Editar Contato \n (5) - Parar\n\n",25*"-","AGENDA",25*"-")
         parar = int(input(":"))
@@ -96,11 +107,17 @@ def loop():
                 listContacts()
                 break
             case 3:
-                removeContacts(lista)
-                break
+                if len(lista) > 0:
+                    removeContacts()
+                    break
+                else:
+                    print("Não existe nenhum número ainda")
             case 4:
-                editContacts()
-                break
+                if len(lista) > 0:
+                    editContacts()
+                    break
+                else:
+                    print("Não existe nenhum número")
             case 5:
                 print("Código parando...")
                 break
